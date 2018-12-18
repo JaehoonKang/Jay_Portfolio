02169c5c27e090fdad6c4c8345aa5461a853c4a4
강재훈 <kangjaehoon@gangjaehunuiMBP.lan>
2018-12-17 19:28:59 -0500

    navigation dropdown

--- js/script.js
+++ js/script.js
@@ -117,6 +117,14 @@ $(document).ready(function(){
         return false;
     });
 
+    $("#navigation li a").click(function(e){
+        e.preventDefault();
+
+        var targetElement = $(this).attr("href");
+        var targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({scrollTop: targetPosition - 50}, "slow")
+    });
+
     const nav = $("#navigation");
     const navTop = nav.offset().top;
 
@@ -129,7 +137,6 @@ $(document).ready(function(){
 
             body.css("padding-top", nav.outerHeight() + "px")
             body.addClass("fixedNav");
-
         }
         else{
             body.css("padding-top", 0)
